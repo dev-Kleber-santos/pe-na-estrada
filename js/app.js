ac6bf73fe9a56990aafb3fb88486085e1ed10d0e
Kleber Santos <klebersantos1289@gmail.com>
2026-01-23 14:01:30 -0300

    Resolvendo conflitos e sincronizando

--- js/app.js
+++ js/app.js
@@ -4,25 +4,24 @@ var btnSignup = document.querySelector("#signup");
 
 var body = document.querySelector("body");
 
+
 btnSignin.addEventListener("click", function () {
     body.className = "sign-in-js";
 });
 
 btnSignup.addEventListener("click", function () {
     body.className = "sign-up-js";
-});
+})
 
-// Dentro do seu app.js
 const loginForm = document.getElementById('loginForm');
 
 if (loginForm) {
-    loginForm.addEventListener('submit', (e) => {
+    loginForm.addEventListener('submit', function (e) {
+
         e.preventDefault();
-        
-        // Aqui você faria a validação de e-mail e senha
+
         console.log("Login realizado com sucesso!");
 
-        // Redireciona para a página inicial
-        window.location.href = "index.html"; 
+        window.location.href = "../index.html"; 
     });
 }
